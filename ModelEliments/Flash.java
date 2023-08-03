9f2fc2106cad633dd9274c57b06fb2447aa0dbef
akhmed <akhmed@storyAkhmedMacBook-Air.local>
2023-08-03 17:41:37 +0300

    5

---
+++ ModelEliments/Flash.java
@@ -0,0 +1,31 @@
+package ModelEliments;
+
+
+import Stuff.Angle3D;
+//import Stuff.Color;
+import java.awt.*;
+import Stuff.Point3D;
+
+public class Flash {
+    public Point3D Location;
+    public Angle3D Angle;
+    public java.awt.Color color;
+    public float Power;
+
+    /**
+     * поворот источников света на угол
+     *
+     * @param angleAction
+     */
+    public void Rotate(Angle3D angleAction) {
+
+    }
+
+    /**
+     * перемещение к точке
+     * @param pointAction
+     */
+    public void Move(Point3D pointAction){
+
+    }
+}
